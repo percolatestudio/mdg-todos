1f89b32810718c8bf85fd49d2f8cb541ac12a4e5
Dominic <dom@percolatestudio.com>
2014-09-03 13:59:57 -0700

    First pass at listsShow for mobile

--- client/templates/listsShow.js
+++ client/templates/listsShow.js
@@ -6,11 +6,11 @@ Template.listsShow.helpers({
   editing: function() {
     return Session.get(EDITING_KEY);
   },
-  
+
   todos: function() {
     return Todos.find({listId: this._id});
   },
-  
+
   checkedClass: function() {
     return this.checked && 'checked';
   }
@@ -20,14 +20,14 @@ Template.listsShow.events({
   'click [data-cancel]': function() {
     Session.set(EDITING_KEY, false);
   },
-  
+
   'submit [name=edit-list-form]': function(e, template) {
     e.preventDefault();
-    
+
     Lists.update(this._id, {$set: {name: template.$('[name=name]').val()}});
     Session.set(EDITING_KEY, false);
   },
-  
+
   'change [name=edit-list]': function(e) {
     if ($(e.target).val() === 'edit') {
       Session.set(EDITING_KEY, true);
@@ -38,26 +38,26 @@ Template.listsShow.events({
     } else {
       if (! Meteor.user())
         throw "Can't change list privacy if not logged in";
-      
+
       if (this.userId)
         Lists.update(this._id, {$unset: {userId: true}});
       else
         Lists.update(this._id, {$set: {userId: Meteor.userId()}});
     }
   },
-  
+
   'submit [name=new-todo-form]': function(e, template) {
     e.preventDefault();
-    
+
     Todos.insert({
-      listId: this._id, 
-      text: template.$('[name=text]').val(),
+      listId: this._id,
+      text: template.$('[type=text]').val(),
       checked: false
     });
-    template.$('[name=text]').val('');
+    template.$('[type=text]').val('');
   },
-  
-  'change [name=checked]': function(e) {
+
+  'change [type=checkbox]': function(e) {
     Todos.update(this._id, {$set: {checked: $(e.target).is(':checked')}});
   }
 });
