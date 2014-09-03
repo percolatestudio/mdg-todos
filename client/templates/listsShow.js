4f29438bdd70ba51a35cc232b3ab8d878c06d679
Dominic <dom@percolatestudio.com>
2014-09-03 15:56:04 -0700

    listsShow functionality fix

--- client/templates/listsShow.js
+++ client/templates/listsShow.js
@@ -21,14 +21,14 @@ Template.listsShow.events({
     Session.set(EDITING_KEY, false);
   },
 
-  'submit [name=edit-list-form]': function(e, template) {
+  'submit .edit-list-form': function(e, template) {
     e.preventDefault();
 
     Lists.update(this._id, {$set: {name: template.$('[name=name]').val()}});
     Session.set(EDITING_KEY, false);
   },
 
-  'change [name=edit-list]': function(e) {
+  'change .edit-list': function(e) {
     if ($(e.target).val() === 'edit') {
       Session.set(EDITING_KEY, true);
     } else if ($(e.target).val() === 'delete') {
