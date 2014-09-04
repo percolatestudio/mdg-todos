78fb12dac4e00d6ec9e837a45a4e480f00731460
Tom Coleman <tom@thesnail.org>
2014-09-04 15:41:09 +1000

    Very basic form errors.

--- client/templates/auth-join.js
+++ client/templates/auth-join.js
@@ -1,13 +1,38 @@
+var ERRORS_KEY = 'joinErrors';
+
+Template.join.helpers({
+  errors: function() {
+    return Session.get(ERRORS_KEY);
+  }
+});
+
 Template.join.events({
   'submit': function(e, template) {
     e.preventDefault();
-    // XXX: check arguments
+    var email = template.$('[name=email]').val();
+    var password = template.$('[name=password]').val();
+    var confirm = template.$('[name=confirm]').val();
+    
+    
+    var errors = [];
+    if (! email)
+      errors.push('Email is required');
+    if (! password)
+      errors.push('Password is required');
+    if (confirm !== password)
+      errors.push('Please confirm your password');
+    
+    Session.set(ERRORS_KEY, errors);
+    if (errors.length)
+      return;
     
     Accounts.createUser({
-      email: template.$('[name=email]').val(),
-      password: template.$('[name=password]').val()
-    }, function() {
-      // XXX: errors
+      email: email,
+      password: password
+    }, function(error) {
+      if (error)
+        return Session.set(ERRORS_KEY, [error.reason]);
+
       Router.go('home');
     });
   }
--- client/templates/auth-signin.js
+++ client/templates/auth-signin.js
@@ -1,15 +1,33 @@
+var ERRORS_KEY = 'signinErrors';
+
+Template.signin.helpers({
+  errors: function() {
+    return Session.get(ERRORS_KEY);
+  }
+});
+
 Template.signin.events({
   'submit': function(e, template) {
     e.preventDefault();
-    // XXX: check arguments
     
-    Meteor.loginWithPassword(
-      template.$('[name=email]').val(),
-      template.$('[name=password]').val(), 
-      function() {
-        // XXX: errors
-        Router.go('home');
-      });
+    var email = template.$('[name=email]').val();
+    var password = template.$('[name=password]').val();
     
+    var errors = [];
+    if (! email)
+      errors.push('Email is required');
+    if (! password)
+      errors.push('Password is required');
+    
+    Session.set(ERRORS_KEY, errors);
+    if (errors.length)
+      return;
+    
+    Meteor.loginWithPassword(email, password, function(error) {
+      if (error)
+        return Session.set(ERRORS_KEY, [error.reason]);
+      
+      Router.go('home');
+    });
   }
 })
