6b822512a61279e1b2756ea8fbce7c0f1de3f0dc
Tom Coleman <tom@thesnail.org>
2014-09-04 14:58:49 +1000

    Hooked up delete button

--- client/templates/todos-item.js
+++ client/templates/todos-item.js
@@ -12,11 +12,21 @@ Template.todosItem.helpers({
 Template.todosItem.events({
   // XXX: alternatively, could just set the class via jQ directly, which might
   //  be conceptually simpler
-  'focus input[type=text]': function() {
+  'focus input[type=text]': function(e) {
     Session.set(EDITING_KEY, this._id);
   },
   
   'blur input[type=text]': function() {
-    Session.set(EDITING_KEY, null);
+    // XXX: have to do this in a timeout to let the click event below fire. 
+    //   is there a better way?
+    var self = this;
+    Meteor.setTimeout(function() {
+      if (Session.equals(EDITING_KEY, self._id))
+        Session.set(EDITING_KEY, null);
+    }, 200);
+  },
+  
+  'click [data-delete-item]': function() {
+    Todos.remove(this._id);
   }
 });
