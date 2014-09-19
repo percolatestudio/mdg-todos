89f305808bcd77b9be26b75144ac6c65ed67d796
Zoltan Olah <zol@percolatestudio.com>
2014-09-19 16:19:46 -0700

    Removes tags and uses _.each in bootstrap for #16

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,56 +1,50 @@
-// pulled from existing project
-
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
   if (Lists.find().count() === 0) {
     var data = [
       {name: "Meteor Principles",
-       contents: [
-         ["Data on the Wire", "Simplicity", "Better UX", "Fun"],
-         ["One Language", "Simplicity", "Fun"],
-         ["Database Everywhere", "Simplicity"],
-         ["Latency Compensation", "Better UX"],
-         ["Full Stack Reactivity", "Better UX", "Fun"],
-         ["Embrace the Ecosystem", "Fun"],
-         ["Simplicity Equals Productivity", "Simplicity", "Fun"]
+       items: ["Data on the Wire",
+         "One Language",
+         "Database Everywhere",
+         "Latency Compensation",
+         "Full Stack Reactivity",
+         "Embrace the Ecosystem",
+         "Simplicity Equals Productivity"
        ]
       },
       {name: "Languages",
-       contents: [
-         ["Lisp", "GC"],
-         ["C", "Linked"],
-         ["C++", "Objects", "Linked"],
-         ["Python", "GC", "Objects"],
-         ["Ruby", "GC", "Objects"],
-         ["JavaScript", "GC", "Objects"],
-         ["Scala", "GC", "Objects"],
-         ["Erlang", "GC"],
-         ["6502 Assembly", "Linked"]
+       items: ["Lisp",
+         "C",
+         "C++",
+         "Python",
+         "Ruby",
+         "JavaScript",
+         "Scala",
+         "Erlang",
+         "6502 Assembly"
          ]
       },
       {name: "Favorite Scientists",
-       contents: [
-         ["Ada Lovelace", "Computer Science"],
-         ["Grace Hopper", "Computer Science"],
-         ["Marie Curie", "Physics", "Chemistry"],
-         ["Carl Friedrich Gauss", "Math", "Physics"],
-         ["Nikola Tesla", "Physics"],
-         ["Claude Shannon", "Math", "Computer Science"]
+       items: ["Ada Lovelace",
+         "Grace Hopper",
+         "Marie Curie", "Physics",
+         "Carl Friedrich Gauss",
+         "Nikola Tesla",
+         "Claude Shannon"
        ]
       }
     ];
 
     var timestamp = (new Date()).getTime();
-    for (var i = 0; i < data.length; i++) {
-      var list_id = Lists.insert({name: data[i].name});
-      for (var j = 0; j < data[i].contents.length; j++) {
-        var info = data[i].contents[j];
+    _.each(data, function(list) {
+      var list_id = Lists.insert({name: list.name});
+
+      _.each(list.items, function(text) {
         Todos.insert({listId: list_id,
-                      text: info[0],
-                      createdAt: new Date(timestamp),
-                      tags: info.slice(1)});
+                      text: text,
+                      createdAt: new Date(timestamp)});
         timestamp += 1; // ensure unique timestamp.
-      }
-    }
+      });
+    });
   }
 });
