479e2f68679eecb06afc07cbe15dde243a0818f5
Zoltan Olah <zol@percolatestudio.com>
2014-09-19 15:50:55 -0700

    Fixes suggestions in todos-item.js for #16

--- client/templates/todos-item.js
+++ client/templates/todos-item.js
@@ -21,14 +21,17 @@ Template.todosItem.events({
   
   'keydown input[type=text]': function(event) {
     // ESC or ENTER
-    if (_.include([27, 13], event.which)) {
+    if (event.which === 27 || event.which === 13) {
       event.preventDefault();
-      $(event.target).blur();
+      event.target.blur();
     }
   },
   
+  // update the text of the item on keypress but throttle the event to ensure
+  // we don't flood the server with updates (handles the event at most once 
+  // every 300ms)
   'keyup input[type=text]': _.throttle(function(event) {
-    Todos.update(this._id, {$set: {text: $(event.target).val()}});
+    Todos.update(this._id, {$set: {text: event.target.value}});
   }, 300),
   
   // handle mousedown instead of click so we don't conflict with the above blur
