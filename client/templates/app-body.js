f4c889e97cdef8b33d5868c298ea672f19644e87
Sashko Stubailo <sashko@mit.edu>
2014-10-07 15:19:52 -0700

    Don't show connection issue within 1 second of starting

--- client/templates/app-body.js
+++ client/templates/app-body.js
@@ -4,6 +4,11 @@ Session.setDefault(MENU_KEY, false);
 var USER_MENU_KEY = 'userMenuOpen';
 Session.setDefault(USER_MENU_KEY, false);
 
+var SHOW_CONNECTION_ISSUE_KEY = 'showConnectionIssue';
+Session.setDefault(SHOW_CONNECTION_ISSUE_KEY, false);
+
+var CONNECTION_ISSUE_TIMEOUT = 1000;
+
 Meteor.startup(function () {
   if (Meteor.isCordova) {
     // set up a swipe left / right handler
@@ -21,6 +26,12 @@ Meteor.startup(function () {
       Session.set(MENU_KEY, true);
     });
   }
+
+  // Don't show the connection error box unless we haven't connected within
+  // 1 second of app starting
+  setTimeout(function () {
+    Session.set(SHOW_CONNECTION_ISSUE_KEY, true);
+  }, CONNECTION_ISSUE_TIMEOUT);
 });
 
 Template.appBody.rendered = function() {
@@ -75,7 +86,11 @@ Template.appBody.helpers({
     }
   },
   connected: function() {
-    return Meteor.status().connected;
+    if (Session.get(SHOW_CONNECTION_ISSUE_KEY)) {
+      return Meteor.status().connected;
+    } else {
+      return true;
+    }
   }
 });
 
